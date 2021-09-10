3b5b8cbf00a8ed9135af786d613faeae32e5fc23
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-09-10 16:52:49 +0200

    tools example

--- tests/test_convolutions.py
+++ tests/test_convolutions.py
@@ -1,79 +1,61 @@
 import logging
+from ogip.tools import convolve
 import os
 import numpy as np
 import pytest
 
 import ogip.spec
+from matplotlib import pyplot as plt   
 
 logging.basicConfig(level=logging.DEBUG)
 
-# Roques & Jourdain 2018
-def crab_ph_cm2_s_kev(en):
-    K=7.417e-4
-    al1=-1.98
-    al2=-2.33
-    Ec=500.
-    f=K*(en/100)**al1*(np.exp(-en/Ec))
-    m=en>Ec*(al1-al2)
-    f[m]=(K*((al1-al2)*Ec/100)**(al1-al2)*(en/100)**al2*np.exp(-(al1-al2)))[m]
 
-    return f
-
-def test_crab(crab_spectra):
+def test_crab_fit(crab_spectra):
     crab_pha_fn, crab_rmf_fn, crab_arf_fn = crab_spectra
     
     crab_pha = ogip.spec.PHAI.from_file_name(crab_pha_fn)
     crab_rmf = ogip.spec.RMF.from_file_name(crab_rmf_fn)
     crab_arf = ogip.spec.ARF.from_file_name(crab_arf_fn)
+            
+    from ogip.tools import plot, transform_rmf, crab_ph_cm2_s_kev    
     
-    ie1 = crab_rmf._energ_lo
-    ie2 = crab_rmf._energ_hi
-    e1 = crab_rmf._e_min
-    e2 = crab_rmf._e_max
-    
-    source=crab_ph_cm2_s_kev(ie1)
+    def gen_line(E0, depth):
+        return lambda x: (x/20)**-2 * (1 - depth * np.exp(-(x-E0)**2/(E0*0.1)**2)/2)
+
+    for E0, rmf_bias, depth in [
+        (25, 1, 1),
+        (60, 5, 1),
+    ]:
+        f = plt.figure(figsize=(15, 10))
         
-    model_spec = np.outer(crab_arf._arf * source*(ie2-ie1),np.ones_like(e1))*crab_rmf._matrix   
-    
-   
-    #csource=np.outer(arf['SPECRESP']*source*(ie2-ie1),np.ones_like(rmf_eb['E_MIN']))*rmf_mt['MATRIX']
-        
-    
-    if True:
-        import matplotlib.pylab as plt
-        plt.figure()
-        plt.step(
-            e1,
-            model_spec.sum(0)/(e2 - e1),
-            where='pre',
-            label="model"
-        )
-        
-        plt.step(
-            e1,
-            crab_pha._rate/(e2 - e1),
-            where='pre',
-            label="data"
-        )
-        
-        plt.loglog()
-        
-        # extra_ticks(
-        #     [20,25,30,35]
-        # )
-        
-        plt.xlim([15, 600])
-        plt.grid()
+        t_rmf = transform_rmf(crab_rmf, 
+                            crab_arf, 
+                            lambda x:(x+rmf_bias), 
+                            preserved_projection=crab_ph_cm2_s_kev)
+
+        plot(crab_pha, crab_ph_cm2_s_kev, crab_rmf, crab_arf, fig=f, label_prefix="crab")    
+        plot(None, crab_ph_cm2_s_kev, t_rmf, crab_arf, fig=f, label_prefix="transformed, crab")
+
+        # Crab spectrum is the same with new and old RMF
+        s = convolve(crab_ph_cm2_s_kev, crab_rmf, crab_arf) 
+        t_s = convolve(crab_ph_cm2_s_kev, t_rmf, crab_arf)
+        assert np.all(np.abs((s - t_s)/s) < 0.001)
+
+        plot(None, gen_line(E0, depth), crab_rmf, crab_arf, fig=f, label_prefix="line")
+        plot(None, gen_line(E0, depth), t_rmf, crab_arf, fig=f, label_prefix="transformed, line", 
+             plot_kwargs=dict(lw=5, alpha=0.8))
+        plot(None, gen_line(E0 - rmf_bias, depth), crab_rmf, crab_arf, fig=f, label_prefix="shifted line")
+
+        plt.savefig(f"conv_{E0}.png")
         plt.savefig("conv.png")
+        
+        # RMF transformation is equivalent to shifting. The match is not exact, but quite close
+        s = convolve(gen_line(E0, depth), crab_rmf, crab_arf) 
+        t_s = convolve(gen_line(E0 - rmf_bias, depth), t_rmf, crab_arf)
+        
+        if E0 < 50: # else shape too affected
+            assert np.nanstd(np.abs((s - t_s)/s)) < 0.05
 
-    # rate_n = np.nansum(spec.data['RATE'][(rmf_eb['E_MIN']>e1) & (rmf_eb['E_MAX']<e2)])
+        t_rmf.to_fits("transformed_rmf.fits")
 
-    # n = csource[:,(rmf_eb['E_MIN']>e1) & (rmf_eb['E_MAX']<e2)].sum()
-    
-    # erg_cm2_per_count = flux_erg_cm2_s_eband/n
-    
-    # print("response norm in", e1,e2,"is",n, "rate norm", rate_n, "erg/cm2 per count", erg_cm2_per_count)
-
-    # return erg_cm2_per_count
-
-    
+        
