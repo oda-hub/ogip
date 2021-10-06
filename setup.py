8c527bb98664c634390407905b30c4c06c087bb9
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-10-06 21:17:44 +0200

    Bump version: 0.1.5-dev0 → 0.1.5

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 
 setup(description="ogip",
       long_description=open('README.md').read(),
-      version='0.1.5-dev0',
+      version='0.1.5',
       include_package_data=True,
       setup_requires=setup_requires)
