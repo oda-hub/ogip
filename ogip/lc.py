43ca2eb2281e3caa04fc446784cc6025a70a6e01
Carlo Ferrigno <ferrigno.carlo@gmail.com>
2024-08-09 18:22:17 +0200

    Correct issues and insert example/test

--- ogip/lc.py
+++ ogip/lc.py
@@ -7,8 +7,7 @@ logger = logging.getLogger()
 
 class LightCurve:
 
-    @staticmethod
-    def from_file_name(fn):
+    def from_file_name(self, fn):
         raise NotImplementedError
 
     def to_long_string(self):
@@ -16,70 +15,76 @@ class LightCurve:
 
     ###
 
-
-
 double_time_definition_keywords = [ ('MJDREFI', 'MJDREFF'), 
                                     ('TSTARTI', 'TSTARTF'),
                                     ('TSTOPI', 'TSTOPF'),
-                                    ('TIMEZERI', 'TIMEZERF')]
+                                    ('TIMEZERI', 'TIMEZERF'),
+                                    ]
 
 single_time_definition_keywords = ['MJDREF',
                                     'TSTART',
                                     'TSTOP',
                                     'TIMEZERO',
-                                    'TIMESYS', 
                                     'TIMEUNIT',
-                                   'CLOCKCOR',
-                                   'TIMEREF',
-                                   'TIMEDEL']
+                                   'TIMEDEL',
+                                   ]
 
-optional_keywords = ['DATE-OBS' , 'DATE-END',
-                                      'TELESCOP', 
-                                      'INSTRUME', 
-                                      'FILTER',
-                                      'OBJECT',
-                                      'RA',
-                                      'DEC',
-                                      'EQUINOX',
-                                      'RADECSYS',
-                                      'ORIGIN',
-                                      'DATE',
-                                      'AUTHOR']
+optional_keywords = ['TELAPSE',
+                    'TIMEREF',                     
+                     'TIMESYS', 
+                     'CLOCKCOR', 
+                     'DATE-OBS' , 
+                     'DATE-END',
+                    'TELESCOP', 
+                    'INSTRUME', 
+                    'FILTER',
+                    'OBJECT',
+                    'RA',
+                    'DEC',
+                    'EQUINOX',
+                    'RADECSYS',
+                    'ORIGIN',
+                    'DATE',
+                    'AUTHOR'
+                    'CREATOR']
 
 
 class rate(LightCurve):
     _time = None
     _timedel = None
-    _rate1 = None
-    _err1 = None
     _fracexp = None
     _keywords = {}
 
     _filename = None
-    
+
     def __init__(self, filename):
 
-        self._keywords = {kk : None for kk in single_time_definition_keywords}
+        self._keywords = {kk : 0.0 for kk in single_time_definition_keywords}
 
         methods_tried= {}
-        for n, m in self.__dict__.items():
+        issue = True
+        for n, m in rate.__dict__.items():
+            
             if n.startswith('from_file_name_'):
                 try:
-                    m.__func__(filename)
+                    self.__getattribute__(m.__name__)(filename)
+                    issue = False
                 except Exception as e:
-                    logger.debug("failed to read with %s.%s %s: %s", self.__name__, n, m, e)
+                    logger.debug("failed to read with %s  %s", m.__name__,  e)
                     methods_tried[n] = e
+        if issue:
+            raise Exception(f"failed to read with any method, tried: {methods_tried}")
 
-        raise Exception(f"failed to read with any method, tried: {methods_tried}")
 
-    
     def read_keywords(self, hdu):
          
         for kk in single_time_definition_keywords + optional_keywords:
+            #print(kk)
             if kk in hdu.header:
                 self._keywords[kk] = hdu.header[kk]
                 logger.debug(f'Updating {kk}')
         for cc in double_time_definition_keywords:
+            #print(cc[0], cc[1])
             if cc[0] in hdu.header and cc[1] in hdu.header:
                 tmp_val = hdu.header[cc[0]] + hdu.header[cc[1]]
                 for kk in single_time_definition_keywords[0:4]:
@@ -93,13 +98,12 @@ class rate(LightCurve):
         f = fits.open(fn)
         issue = True
         for e in f:
-            try:
-                    
+            try:                   
                     self._filename = fn
                     self._time=e.data['TIME']
                     self._timedel=e.data['TIMEDEL']
-                    self._rate1=e.data['RATE']
-                    self._err1=e.data['ERROR']
+                    self._rate=e.data['RATE']
+                    self._error=e.data['ERROR']
 
                     self.read_keywords(e)
                     # set if I find an extension
@@ -115,7 +119,7 @@ class rate(LightCurve):
         try:
             hdu = f['RATE']
         except:
-            logger.error('Unable to find the extenision RATE in ' + fn)
+            logger.debug('Unable to find the extenision RATE in ' + fn)
             raise Exception('Unable to find the extenision RATE in ' + fn)
         
         self.read_keywords(hdu)
@@ -136,40 +140,34 @@ class rate(LightCurve):
             self._timedel = self._time[1:] - self._time[0:-1]
             self._timedel = np.append(self._timedel, self._timedel[-1])
         
-        if 'RATE' in hdu.data.names:
-            self._rate1 = hdu.data['RATE']
-        elif 'COUNTS' in hdu.data.names:
-            self._rate1 = hdu.data['COUNTS']
-        else:
-            rate_columns = []
-            for kk in hdu.data.names:
-                if kk.startswith('RATE'):
-                    rate_columns.append(kk)
-
-            if len(rate_columns) == 0 :
-                logger.error(f'No RATE or COUNTS in {fn}')
-                raise Exception(f'No RATE or COUNTS in {fn}')
-            else:
-                for kk in rate_columns:
-                    self.__setattr__(f'_{kk.lower()}', hdu.data[kk])
         
-        if 'ERROR' in hdu.data.names:
-            self._err1 = hdu.data['ERROR']
-        else:
-            err_columns = []
-            for kk in hdu.data.names:
-                if kk.startswith('ERROR'):
-                    err_columns.append(kk)
+        rate_columns = []
+        for kk in hdu.data.names:
+            if kk.startswith('RATE') or kk.startswith('COUNTS'):
+                rate_columns.append(kk)
 
-            if len(err_columns) == 0 :
-                logger.warning(f'No Errors in {fn} assuming Poissonian')
-                for kk_r in rate_columns:
-                    err_kk = kk_r.replace('RATE', 'ERROR')
-                    self.__setattr__(f'_{err_kk.lower()}', np.sqrt(hdu.data[kk_r]))
-                
-            else:
-                for kk in err_columns:
-                    self.__setattr__(f'_{kk.lower()}', hdu.data[kk])
+        if len(rate_columns) == 0 :
+            logger.error(f'No RATE or COUNTS in {fn}')
+            raise Exception(f'No RATE or COUNTS in {fn}')
+        else:
+            for kk in rate_columns:
+                self.__setattr__(f'_{kk.lower()}', hdu.data[kk])
+        
+        
+        err_columns = []
+        for kk in hdu.data.names:
+            if kk.startswith('ERROR'):
+                err_columns.append(kk)
+
+        if len(err_columns) == 0 :
+            logger.warning(f'No Errors in {fn} assuming Poissonian')
+            for kk_r in rate_columns:
+                err_kk = kk_r.replace('RATE', 'ERROR').replace('COUNTS', 'ERROR')
+                self.__setattr__(f'_{err_kk.lower()}', np.sqrt(hdu.data[kk_r]))
+            
+        else:
+            for kk in err_columns:
+                self.__setattr__(f'_{kk.lower()}', hdu.data[kk])
             
         
         if 'FRACEXP' in hdu.data.names:
@@ -178,17 +176,22 @@ class rate(LightCurve):
             self._fracexp = np.ones(len(self._rate))
 
 
-    @property
+    
     def lc_hdu(self):
         # https://heasarc.gsfc.nasa.gov/docs/heasarc/ofwg/docs/rates/ogip_93_003/ogip_93_003.html
 
         columns = [
-                    fits.Column(name='RATE', array=self._rate, format='1E'),
-                    fits.Column(name='ERROR', array=self._stat_err, format='1E'),
-                    fits.Column(name='TIME', array=self._sys_err, format='D'),
+                    fits.Column(name='TIME', array=self._time, format='D'),
                     fits.Column(name='TIMEDEL', array=self._timedel, format='D'),
                     fits.Column(name='FRACEXP', array=self._fracexp, format='1E'),
                 ]
+        
+        for n, m in self.__dict__.items():
+            #print(n)
+            if n.startswith('_rate') or n.startswith('_error'):
+                columns.append(fits.Column(name=n.upper().replace('_',''), array=m, format='1E'))
+
+        
         header = fits.Header(cards=dict(
                                       EXTNAME="RATE", # - the name (i.e. type) of the extension
                                       TELESCOP="", # - the "telescope" (i.e. mission/satellite name).
@@ -208,20 +211,21 @@ class rate(LightCurve):
                                       ORIGIN  = '',#         / who produced the fits file
                                       DATE    = '', #         / when the fits file was created
                                       AUTHOR  = '',   #  /name of program that produced this file
-                                      ))
+                                      MJDREF = 0.0))
         for kk in ['DATE-OBS' , 'DATE-END']:
             header[kk] = ''
-        for kk, vv in self._keywords:
+        for kk, vv in self._keywords.items():
             header[kk] = vv
-
+            #print(kk, vv)
+        #print(header)
         return fits.BinTableHDU.from_columns(columns, header=header)
 
     def to_fits(self, fn: str):
         logger.info("store to fits %s %s", self, fn)
-
+        #print(self.lc_hdu())
         fits.HDUList([
                 fits.PrimaryHDU(),
-                self.spectrum_hdu,
+                self.lc_hdu(),
             ]).writeto(fn, overwrite=True)
 
     @property
